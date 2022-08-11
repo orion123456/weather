00a7a33623ab9cfb835bebd7fa61c0e47fa0ad4d
Oleg <lozovoi_oleg@mail.ru>
2022-08-11 15:35:45 +0300

    edit timeZone

--- src/App.svelte
+++ src/App.svelte
@@ -2,11 +2,8 @@
 	import moment from "moment";
 	import 'moment/locale/ru'
 
-	//Изменить код так чтобы при загрузке страницы данные сразу же отображались
-	//После отправки формы тоже
-	//Вывести день недели и время
 	//Почитать про промисы и про async await https://www.youtube.com/watch?v=5kAPExqSZ1I
-	//Вывести время в зависимости от таймзоны timezone
+
 
 	const appid = '77d3010e8f2da851498d867aa5c6e12f'
 	const units = 'metric'
@@ -21,6 +18,10 @@
 	let weather = []
 	let name
 
+	let value = `${lat}, ${lon}`
+
+	let timeZone
+
 	function getWeather() {
 		const url = `${endpoint}?appid=${appid}&units=${units}&lang=${lang}&lat=${lat}&lon=${lon}`
 
@@ -32,43 +33,35 @@
 					temp = data.main.temp.toFixed(0)
 					weather = data.weather
 					name = data.name
-					console.log(data)
+					timeZone = moment().utcOffset(data.timezone/60).format('dddd, HH:mm')
 				});
 	}
 
-	function handleSubmit(event) {
-		const value = event.currentTarget.coords.value
+	function handleSubmit() {
 		let value1 = value.split(', ')
 		lat = value1[0]
 		lon = value1[1]
 
 		getWeather()
+
 	}
 
 	getWeather()
-
-	let weekTime = moment().locale('ru').format('dddd, HH:mm')
-
-	let timeZone = moment().utcOffset(180);
-	console.log(timeZone)
 
 </script>
 
 <h1>Виджет погоды</h1>
 <form on:submit|preventDefault={handleSubmit}>
-	<input type="text" name="coords">
+	<input type="text" name="coords" bind:value>
 	<button>Получить погоду</button>
 </form>
 
-
 <div>{name}</div>
-<div>{weekTime}</div>
+<div>{timeZone}</div>
 <div>{temp}°C</div>
 {#each weather as item}
 	<div>{item.description}</div>
 {/each}
-
-
 
 <style>
 
